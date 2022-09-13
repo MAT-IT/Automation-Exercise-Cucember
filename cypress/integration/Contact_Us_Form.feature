daf9aa7c7b276d761ebf7aa4fa62ca74ff8c820c
MAT <77358430+matzhd@users.noreply.github.com>
2022-09-13 18:58:53 +0100

    tested contact us form

---
+++ cypress/integration/Contact_Us_Form.feature
@@ -0,0 +1,17 @@
+Feature: Contact Us Form
+ 
+    Background: set up environment
+        Then get data from Fixtures        
+   
+    Scenario: TC06 Contact Us Form   
+        And Navigate to url
+        And Verify that home page is visible successfully
+        And Click on 'Contact Us' button
+        And Verify 'GET IN TOUCH' is visible
+        And Enter name, email, subject and message
+        And Upload file And Click 'Submit' button 
+        And Click OK button
+        And Verify success message 'Success! Your details have been submitted successfully.' is visible
+        And Click 'Home' button and verify that landed to home page successfully
+
+       
---
+++ cypress/integration/Contact_Us_Form/Contact_Us_Form.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+import { Given, And, Then } from "cypress-cucumber-preprocessor/steps";
+import HomePage from "../PageObjectRepo/HomePage"
+import contactUsPage from "../PageObjectRepo/ContactUsPage";
+
+const homepage = new HomePage();
+const contactUs = new contactUsPage;
+
+Then("get data from Fixtures", function () {
+    cy.fixture("data").then(
+        function (data) {
+            this.data = data
+        }
+    )
+})
+Given("Navigate to url", function () {
+    cy.visit(this.data.url)
+})
+
+Given("Verify that home page is visible successfully", () => {
+    cy.title().should("contain", "Automation Exercise")
+    
+})    
+Given("Click on 'Contact Us' button",()=>{
+    homepage.ContactUsBotton().click()
+})
+
+Given("Verify 'GET IN TOUCH' is visible",()=>{
+    contactUs.GetInTouch().should("be.visible")
+})
+Given("Enter name, email, subject and message",()=>{
+    contactUs.name().type("mat");
+    contactUs.email().type("mat@gmail.com")
+    contactUs.subject().type("tell off")
+    contactUs.message().type("your bird is distrub us when we studie lesson")
+})
+Given("Upload file And Click 'Submit' button",()=>{
+    contactUs.selectFile().attachFile("nature.jpeg")
+    contactUs.submitBtn().click().wait(5000)
+})
+Given("Verify success message 'Success! Your details have been submitted successfully.' is visible",()=>{
+    contactUs.status().should("have.text","Success! Your details have been submitted successfully.")
+})
+Given("Click 'Home' button and verify that landed to home page successfully",()=>{
+    contactUs.backhomepage().click()
+    homepage.logo().should("be.visible")
+})
+
+
