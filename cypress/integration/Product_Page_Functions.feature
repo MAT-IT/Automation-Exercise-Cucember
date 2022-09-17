2954bd306c58000be01231be006a94fe3fe77d83
MAT <77358430+matzhd@users.noreply.github.com>
2022-09-17 11:19:00 +0100

    created test cases 21

--- cypress/integration/Product_Page_Functions.feature
+++ cypress/integration/Product_Page_Functions.feature
@@ -37,13 +37,40 @@ Feature: Contact Us Form
         And Click 'Add to cart' button
         And Click 'View Cart' button
         And Verify that product is displayed in cart page with exact quantity
+    
+    Scenario: TC18 View Category Products  
+        And Verify that categories are visible on left side bar
+        And Click on 'Women' category
+        And Click on any category link under 'Women' category, for example: Dress
+        And Verify that category page is displayed and confirm text 'WOMEN - TOPS PRODUCTS'
+        And On left side bar, click on any sub-category link of 'Men' category
+        And Verify that user is navigated to that category page 
 
-     @focus
-    Scenario: TC13 Verify Product quantity in Cart
+    
+    Scenario: TC19 View & Cart Brand Products
+        And Click on 'Products' button
+        And Verify that Brands are visible on left side bar
+        And Click on whole brand name and verify that user is navigated to brand page and brand products are displayed
         
-        And Click on 'View Product' of first product
-        And Verify product detail is opened
-        And Increase quantity to 4
-        And Click 'Add to cart' button
-        And Click 'View Cart' button
-        And Verify that product is displayed in cart page with exact quantity
+    @focus
+    Scenario: TC20 Search Products and Verify Cart After Login
+        And Click on 'Products' button
+        And Verify user is navigated to ALL PRODUCTS page successfully
+        And Enter product name in search input and click search button
+        And Verify 'SEARCHD PRODUCTS' is visible
+        And Verify all the products related to search are visible
+        And Add those products to cart
+        And click wiev cart button
+        And click login and submit
+        And Again, go to Cart page
+        And Verify that those products are visible in cart after login as well
+
+    
+    Scenario: TC21 Add review on product
+       And Click on 'Products' button
+       And Verify user is navigated to ALL PRODUCTS page successfully
+       And Click on 'View Product' button
+       And Verify 'Write Your Review' is visible
+       And Enter name, email and review
+       And Click 'Submit' button
+       And Verify success message 'Thank you for your review.'
--- cypress/integration/Product_Page_Functions/Product_Page_Functions.js
+++ cypress/integration/Product_Page_Functions/Product_Page_Functions.js
@@ -4,16 +4,13 @@ import HomePage from "../PageObjectRepo/HomePage"
 import productPage from "../PageObjectRepo/ProductPage"
 import searchProductPage from "../PageObjectRepo/SearchProductPage"
 import CartPage from "../PageObjectRepo/CartPage"
+import { commonVariable  } from '../helper'; 
 
 const homepage = new HomePage();
 const productpage = new productPage();
 const searchproduct= new searchProductPage()
 const cartpage =new CartPage()
 
-
-Given("Verify that home page is visible successfully",()=>{
-    cy.title("includes","Automation")
-})
 Given("Click on 'Products' button",()=>{
     homepage.productbtn().click()
 })
@@ -117,4 +114,106 @@ Given("Increase quantity to 4",()=>{
  Given("Verify that product is displayed in cart page with exact quantity",()=>{
     cartpage.cartProductImage().should("be.visible")
  })
+
+
+ Given("Verify that categories are visible on left side bar",()=>{
+    homepage.category().should("be.visible")
+ })
+ Given("Click on 'Women' category",()=>{
+    productpage.categoryWomen().click()
+ })
+ Given("Click on any category link under 'Women' category, for example: Dress",()=>{
+    productpage.categoryWomenDress().click({force: true})
+ })
+ Given("Verify that category page is displayed and confirm text 'WOMEN - TOPS PRODUCTS'",()=>{
+    productpage.productTitle().should("be.visible")
+ })
+ Given("On left side bar, click on any sub-category link of 'Men' category",()=>{
+    productpage.categoryMan().click()
+    productpage.categoryManTshirts().click({force: true})
+ })
+ Given("Verify that user is navigated to that category page",()=>{
+    productpage.productTitle().should("be.visible")    
+ })
+
+
+
+ Given("Verify that Brands are visible on left side bar",()=>{
+    productpage.brandsHeader().should("be.visible")   
+ })
+ Given("Click on whole brand name and verify that user is navigated to brand page and brand products are displayed",()=>{
+    productpage.brandsName().then(($el) => { 
+        const itemCount = Cypress.$($el).length;
+        cy.log(itemCount)
+        for (let i = 0 ; i< itemCount;i++){
+            productpage.brandsName().eq(i).then(function(item){
+                let brandname = item.text()
+                brandname =brandname.replace(brandname.substring(0,4),"")       
+                cy.log(brandname)
+                cy.wrap(brandname).as("brandname")
+               })
+               productpage.brandsName().eq(i).click()
+               //Verify that user is navigated to brand page and brand products are displayed
+                productpage.productTitle().then(function(item){
+                let title =item.text()
+                cy.log(title)
+               expect(title).to.contain(this.brandname)
+            })
+        } 
+    })
+ })
+
+ Given("Add those products to cart",()=>{
+    
+    searchproduct.productNameList()
+             .then(function(item){
+                 const itemCount=Cypress.$(item).length;
+                 cy.log(itemCount)
+                 for (var i = 0; i < itemCount; i++) {
+                     searchproduct.productList()
+                         .eq(i).should('contain.text', "Jeans")
+                     //urun isimlerini al arraye at
+                     var name = item.eq(i).text()
+                     commonVariable.productName.push(name)
+
+                     //Add those products to cart
+                     productpage.allproductsforHover().eq(i).realHover('mouse')
+                     productpage.addToCartAfterHover().eq(i).click({force: true})
+                 
+                }
+             })
+    commonVariable.productName.sort()      
+     cy.log(commonVariable.productName)   
+ })
+ Given("Again, go to Cart page",()=>{
+    homepage.cartBtn().click() 
+ })
+ Given("click wiev cart button",()=>{
+    productpage.ViewCartBtn().click() 
+ })
+ Given("click login and submit",()=>{
+    cartpage.SignUploginButton().click()
+        cy.LogIn()
+ })
+ Given("Verify that those products are visible in cart after login as well",()=>{
+    
+     cartpage.cartproductNameList().then(function(items){
+         const itemCount=Cypress.$(items).length;
+         for (let i = 0; i < itemCount; i++) {
+             var name = items.eq(i).text()
+             commonVariable.cartNameArr.push(name)                
+         }
+         cy.log(commonVariable.cartNameArr)
+         expect(commonVariable.productName).to.be.eql(commonVariable.cartNameArr)
+     })
+     //commonVariable.cartNameArr.sort()
+     cy.log(commonVariable.productName) 
+     cy.log(commonVariable.cartNameArr)
+     
+     
+ })
+
+ Given("Verify 'SEARCHD PRODUCTS' is visible",()=>{
+    productpage.productTitle().should("be.visible")
+})
  
